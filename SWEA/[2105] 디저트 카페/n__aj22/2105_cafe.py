b05ae1673341df440e9cafab678a9bf4c944aafe
Jihae Na <njnjh0317@gmail.com>
2020-10-07 21:53:06 +0900

    n__aj22 2105 done

--- SWEA/[2105] 디저트 카페/n__aj22/2105_cafe.py
+++ SWEA/[2105] 디저트 카페/n__aj22/2105_cafe.py
@@ -1,6 +1,5 @@
 import sys
 sys.stdin = open("/Users/najihye/algo2/SWEA/[2105] 디저트 카페/n__aj22/input.txt", "r")
-flag = False
 cafe_num = -1
 dx = [1, -1, -1, 1]
 dy = [1, 1, -1, -1]
@@ -8,15 +7,14 @@ def dfs(cafe, direction, starty,startx, nowy, nowx, visited, num, N, turn):
     global flag
     global cafe_num
     if(startx == nowx and starty == nowy and turn == 4):
-        flag = True
         cafe_num = max(cafe_num, num)
         return
     if(cafe[nowy][nowx] in visited): # 방문한 적 있는 곳이면
         return
     else:  
+        visited.append(cafe[nowy][nowx])#방문한 곳으로 추가해줌
         nextx = nowx + dx[direction]
         nexty = nowy + dy[direction]
-        visited.append(cafe[nowy][nowx])#방문한 곳으로 추가해줌
         if not (nextx == N or nexty == N or nextx == -1 or nexty == -1):
             dfs(cafe, direction, starty,startx,  nexty, nextx, visited, num+1, N, turn)      
                 
@@ -37,7 +35,6 @@ for test_case in range(1, T + 1):
     cafe_num = -1
     for i in range(N):
         for j in range(N):
-            flag = False
             dfs(cafe, 0,i, j,i,j,[], 0,N, 0)
     print("#"+str(test_case)+" "+str(cafe_num))
     
