97a3e4d3f19b849e1322c034d9aad30a1713876f
JungDayoon <45536712+JungDayoon@users.noreply.github.com>
2020-10-10 23:35:56 +0900

    Update 파이프옮기기_dp.py

--- BOJ/[17070] 파이프 옮기기1/ekdbsl/파이프옮기기_dp.py
+++ BOJ/[17070] 파이프 옮기기1/ekdbsl/파이프옮기기_dp.py
@@ -19,5 +19,5 @@ for j in range(2, N):
             pipeCnt[i][j][1] += pipeCnt[i][j-1][1] + pipeCnt[i][j-1][2]
         if isIn(i-1, j-1) and Map[i-1][j-1] == 0 and Map[i-1][j] == 0 and Map[i][j-1] == 0:
             pipeCnt[i][j][2] += pipeCnt[i-1][j-1][0] + pipeCnt[i-1][j-1][1] + pipeCnt[i-1][j-1][2]
-# dfs(0, 1, 1)
+            
 print(sum(pipeCnt[N-1][N-1]))
