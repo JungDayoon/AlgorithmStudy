75fb09dd3b357d31010ef08bf35110aec1064326
Jihae Na <njnjh0317@gmail.com>
2020-10-11 14:31:55 +0900

    n__aj22 17070 done

---
+++ BOJ/[17070] 파이프 옮기기1/n__aj22/17070_pipe.py
@@ -0,0 +1,25 @@
+N = int(input())
+arr =[]
+for i in range(N):
+    arr.append(list(map(int, input().split())))
+arr2 = [[0]*N for _ in range(N)]
+check = [[] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        check[i].append([0,0,0])
+
+for i in range(1, N):
+    if(arr[0][i] == 1):
+        break
+    check[0][i] = [0,0,1]
+
+for i in range(1,N):
+    for j in range(1, N):
+        if(arr[i][j] == 1):
+            continue
+        check[i][j][0] = check[i-1][j][0]+check[i-1][j][1]
+        if(arr[i-1][j-1] == 0 and arr[i-1][j] == 0 and arr[i][j-1] == 0):
+            check[i][j][1] = (check[i - 1][j - 1][0] + check[i - 1][j - 1][1] + check[i - 1][j - 1][2])
+        check[i][j][2] = (check[i][j-1][1] + check[i][j-1][2])
+result = sum(check[N-1][N-1])
+print(result)
