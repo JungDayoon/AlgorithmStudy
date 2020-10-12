4308a78c9f7f2e17686c106286d14d5c7046e6d6
Jihae Na <njnjh0317@gmail.com>
2020-10-12 18:13:22 +0900

    n__aj22 14500 done

--- BOJ/[14500] 테트로미노/n__aj22/14500_tetromino.py
+++ BOJ/[14500] 테트로미노/n__aj22/14500_tetromino.py
@@ -20,26 +20,36 @@ def isIn(nowr, nowc):
 def DFS(nowr, nowc, num, num_list):
     global max_answer
     #if(num==4):
+
     if(len(num_list)==4):
-        print(num_list, sum(num_list))
         max_answer = max(max_answer, sum(num_list))
         return
-
-
-
-    for i in range(2):
+    for i in range(4):
         nextr = nowr + dr[i]
         nextc = nowc + dc[i]
-
         if(isIn(nextr, nextc)):
             num_list.append(arr[nextr][nextc])
             visited[nextr][nextc] = False
             DFS(nextr, nextc, num+1, num_list)
+            if (i<3):
+                nextr2 = nowr + dr[i + 1]
+                nextc2 = nowc + dc[i + 1]
+                if (isIn(nextr2, nextc2) and num<3):
+                    num_list.append(arr[nextr2][nextc2])
+                    visited[nextr2][nextc2] = False
+                    DFS(nextr, nextc, num+2, num_list)
+                    DFS(nextr2, nextc2, num+2, num_list)
+                    num_list.pop(-1)
+                    visited[nextr2][nextc2] = True
             num_list.pop(-1)
             visited[nextr][nextc] = True
+
+
     return
 
 for i in range(N):
     for j in range(M):
-        DFS(i, j, 0, [])
+        visited[i][j]=False
+        DFS(i, j, 1, [arr[i][j]])
+        visited[i][j]=True
 print(max_answer)
