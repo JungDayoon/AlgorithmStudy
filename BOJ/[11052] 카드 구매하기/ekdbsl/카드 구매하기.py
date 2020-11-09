7ed794ebfa0b44dfa736897813c74a60adb0bd0c
Jungdayoon <dayoon98@naver.com>
2020-11-09 17:50:05 +0900

    boj 11052 readme

--- BOJ/[11052] 카드 구매하기/ekdbsl/카드 구매하기.py
+++ BOJ/[11052] 카드 구매하기/ekdbsl/카드 구매하기.py
@@ -5,7 +5,7 @@ dp = [0 for _ in range(N+1)]
 dp[1] = price[1]
 
 for i in range(2, N+1):
-    for j in range(0, i+1):
+    for j in range(0, i):
         dp[i] = max(dp[i], dp[j] + price[i-j])
 
 print(dp[N])
