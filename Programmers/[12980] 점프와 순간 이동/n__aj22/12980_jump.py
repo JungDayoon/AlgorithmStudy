f150d2820315edcd5f4ae7c3a134d83293e5918b
Jihae Na <njnjh0317@gmail.com>
2021-01-23 15:56:15 +0900

    n__aj22 12980 done

--- Programmers/[12980] 점프와 순간 이동/n__aj22/12980_jump.py
+++ Programmers/[12980] 점프와 순간 이동/n__aj22/12980_jump.py
@@ -8,6 +8,19 @@ def solution(n):
 
     return dp[N]
 
+def recursive(n):
+    cnt = 0
+    while(n>0):
+        if(n%2 == 0):
+            n = n//2
+        else:
+            n = n-1
+            cnt+=1
+    return cnt
+
 if __name__ == "__main__":
     N = int(input())
-    print(solution(N))
+    arr = [-1 for i in range(N+1)]
+    arr[0] = 1
+    arr[1] = 1
+    print(recursive(N))
