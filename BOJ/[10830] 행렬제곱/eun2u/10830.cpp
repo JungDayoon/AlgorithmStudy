8d9d3d02217f4dfbe154b56aa0209fc859d17a9d
eun2u <hhaneunjoo@gmail.com>
2021-02-12 18:32:58 +0900

    [10830] 행렬제곱 eun2u cpp commit

--- BOJ/[10830] 행렬제곱/eun2u/10830.cpp
+++ BOJ/[10830] 행렬제곱/eun2u/10830.cpp
@@ -5,38 +5,32 @@
 using namespace std;
 int N;
 long long B;
+vector<vector<int> > multiple(vector<vector<int> > arr1, vector<vector<int> > arr2){
+    vector<vector<int> > ret(N, vector<int>(N,0));
 
-vector<vector<int> > result;
-vector<vector<int> > square(vector<vector<int> > arr, long long b){
-    if(b==1)    return arr;
-    
-    vector<vector<int> > nArr(N, vector<int>(N,0));
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             long long num=0;
             for(int a=0;a<N;a++){
-                num+=arr[i][a]*arr[a][j];
+                num+=arr1[i][a]*arr2[a][j];
             }
-            nArr[i][j]=num%1000;
+            ret[i][j]=num%1000;
         }
     }
-
+    return ret;
+}
+vector<vector<int> > square(vector<vector<int> > arr, long long b){
+    if(b==1)    
+        return arr;
+    
+    vector<vector<int> > nArr = multiple(arr,arr);
     nArr=square(nArr,b/2);    
+    
     if(b%2==0)
         return nArr;
     
     else{//홀수일때
-        vector<vector<int> > result(N, vector<int>(N,0));
-
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                long long num=0;
-                for(int a=0;a<N;a++){
-                    num+=nArr[i][a]*arr[a][j];
-                }
-                result[i][j]=num%1000;
-            }
-        }     
+        vector<vector<int> > result = multiple(nArr,arr);
         return result;
     }
 }
@@ -49,7 +43,8 @@ int main(){
         for(int j=0;j<N;j++)
             cin>>arr[i][j];
     
-    result=square(arr,B);
+    vector<vector<int> > result=square(arr,B);
+
     for(int i=0;i<N;i++){
         for(int j=0;j<N;j++){
             cout<<result[i][j]%1000<<" ";
