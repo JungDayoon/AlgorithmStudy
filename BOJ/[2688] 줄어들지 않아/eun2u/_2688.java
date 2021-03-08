d50c89e039c7059f8e4acd6de4b748f4b8eb902d
eun2u <hhaneunjoo@gmail.com>
2021-03-08 15:35:43 +0900

    [2688] 줄어들지 않아 eun2u java commit

---
+++ BOJ/[2688] 줄어들지 않아/eun2u/_2688.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+
+public class _2688 {
+    
+    static long[][] digit=new long[65][65];
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int TC=Integer.parseInt(br.readLine());
+        
+        for(int tc=0;tc<TC;tc++){
+            int N=Integer.parseInt(br.readLine());
+            
+            long ret=findLDS(N);
+            System.out.println(ret);
+            
+        }
+    }
+    public static long findLDS(int N){
+        long result=0;
+        
+        for(int i=0;i<=9;i++)
+            digit[1][i]=1; //자리수 1개인데 i로 끝나는 경우
+
+        for(int d=1;d<=N;d++){
+            if(digit[d][0]>0) continue;
+            result=0;
+
+            digit[d][0]=digit[d-1][0];
+            result+=digit[d][0];
+            for(int i=1;i<10;i++){    
+                digit[d][i]=digit[d][i-1]+digit[d-1][i];
+                result+=digit[d][i];
+            }
+        }
+
+        return result;
+    }
+}
