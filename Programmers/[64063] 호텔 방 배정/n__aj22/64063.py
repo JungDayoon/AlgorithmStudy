9c04570ccca04f448eb1b408abafbbd77a6e1ca5
Jihae Na <njnjh0317@gmail.com>
2021-03-26 16:52:39 +0900

    n__aj22 64063 done

--- Programmers/[64063] 호텔 방 배정/n__aj22/64063.py
+++ Programmers/[64063] 호텔 방 배정/n__aj22/64063.py
@@ -1,6 +1,8 @@
 #정확성 26/26
 #효율성 0/7
-def solution(k, room_number):
+import sys
+sys.setrecursionlimit(10**5)
+def first_solution(k, room_number):
     answer = []
     next_room = [0]
     for i in range(1, k+1):
@@ -22,7 +24,22 @@ def solution(k, room_number):
                 next_room[chk_num] = next_room[chk_num+1]
                 chk_num = next_chk_num
     return answer
+
+def findemptyroom(emptyroom, num):
+    if num not in emptyroom.keys():
+        emptyroom[num] = num+1
+        return num
+    empty = findemptyroom(emptyroom, emptyroom[num])
+    emptyroom[num] = empty+1
+    return empty
+def solution(k, room_number):
+    answer = []
+    emptyroom = {}
+
+    for num in room_number:
+        answer.append(findemptyroom(emptyroom, num))
+    return answer
 if __name__ == "__main__":
-    k = 1
-    room_number = [1]
+    k = 10
+    room_number = [1,3,4,1,3,1]
     print(solution(k, room_number))
