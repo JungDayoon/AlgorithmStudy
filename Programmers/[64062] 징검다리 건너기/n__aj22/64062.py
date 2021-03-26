2b85ba43b7da6fd718f3abde5d437730f2f1b661
Jihae Na <njnjh0317@gmail.com>
2021-03-26 17:57:22 +0900

    n__aj22 64062 done

---
+++ Programmers/[64062] 징검다리 건너기/n__aj22/64062.py
@@ -0,0 +1,29 @@
+def possibility(num, stones, k):
+    count = 0
+    for stone in stones:
+        if(stone-num<=0):
+            count+=1
+        else:
+            count = 0
+        if(count==k):
+            return False
+    return True
+def solution(stones, k):
+    answer = 0
+    end = max(stones)
+    start = 0
+    while(True):
+        if(start == end - 1):
+            answer = start+1
+            break
+        mid = (start+end)//2
+        if(possibility(mid, stones, k)):
+            start = mid
+        else:
+            end = mid
+
+    return answer
+if __name__ == "__main__":
+    stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
+    k = 3
+    print(solution(stones, k))
