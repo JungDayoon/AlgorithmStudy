43aa722e9a688c5b4afc2be668d287967f86ce70
Jihae Na <njnjh0317@gmail.com>
2021-04-02 17:02:28 +0900

    n__aj22 1715 done

---
+++ BOJ/[1715] 카드 정렬하기/n__aj22/1715.py
@@ -0,0 +1,22 @@
+import heapq
+def calculate(arr):
+    answer = 0
+    while(arr):
+        if(len(arr)==1):
+            return answer
+        num1 = heapq.heappop(arr)
+        num2 = heapq.heappop(arr)
+        sum_num = num1+num2
+        answer+=sum_num
+        heapq.heappush(arr, sum_num)
+    return answer
+if __name__ == "__main__":
+    N = int(input())
+    queue = []
+
+    for i in range(1, N+1):
+        num = int(input())
+        heapq.heappush(queue, num)
+    
+    print(calculate(queue))
+
