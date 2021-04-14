e7a207fab2b58e558736f86000dcca6631db1d76
Jihae Na <njnjh0317@gmail.com>
2021-04-14 11:00:08 +0900

    n__aj22 16946 done

--- BOJ/[16946] 벽 부수고 이동하기 4/n__aj22/16946.py
+++ BOJ/[16946] 벽 부수고 이동하기 4/n__aj22/16946.py
@@ -5,23 +5,23 @@ def isin(y, x):
     if(0<=y<N and 0<=x<M):
         return True
     return False
-def bfs(y, x, visited):
+def bfs(y, x, visited, index):
     check = []
     queue = collections.deque()
-    visited[y][x] = 0
+    visited[y][x] = [0,0]
     queue.append([y, x])
     while(queue):
         y, x = queue.popleft()
         check.append([y, x])
         for i in range(4):
             nexty, nextx = y+dy[i], x + dx[i]
-            if isin(nexty, nextx) and visited[nexty][nextx] == -1 and arr[nexty][nextx] == '0':
-                visited[nexty][nextx] = 0
+            if isin(nexty, nextx) and visited[nexty][nextx][0] == -1 and arr[nexty][nextx] == '0':
+                visited[nexty][nextx][0] = 0
                 queue.append([nexty, nextx])
     for point in check:
         y, x = point
-        visited[y][x] = len(check)
-    return
+        visited[y][x] = [len(check), index]
+    return 
 
 if __name__ == "__main__":
     N, M = map(int, input().split())
@@ -31,19 +31,24 @@ if __name__ == "__main__":
     visited = [[-1]*M for _ in range(N)]
     for i in range(N):
         for j in range(M):
-            if(arr[i][j] == '0' and visited[i][j] == -1):
-                bfs(i, j, visited)
-    for i in visited:
-        print(i)
-        
+            visited[i][j] = [-1, -1]
+    cnt = 0
     for i in range(N):
         for j in range(M):
-            if(visited[i][j] == -1):
+            if(arr[i][j] == '0' and visited[i][j][0] == -1):
+                bfs(i, j, visited, cnt)
+                cnt+=1
+
+    for i in range(N):
+        for j in range(M):
+            if(visited[i][j][0] == -1):
                 answer = 1
+                check = []
                 for k in range(4):
                     nexty, nextx = i+dy[k], j+dx[k]
-                    if(isin(nexty, nextx) and visited[nexty][nextx]!=-1):
-                        answer+=visited[nexty][nextx]
+                    if(isin(nexty, nextx) and visited[nexty][nextx][0]!=-1 and visited[nexty][nextx][1] not in check):
+                        answer+=visited[nexty][nextx][0]
+                        check.append(visited[nexty][nextx][1])
                 arr[i][j] = str(answer%10)
             else:
                 arr[i][j] = '0'
