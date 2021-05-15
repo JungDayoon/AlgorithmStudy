495d9e328f5104accfc295c047306556d64ac560
Jihae Na <njnjh0317@gmail.com>
2021-05-15 16:08:29 +0900

    n__aj22 16929 done

---
+++ BOJ/[16929] Two Dots/n__aj22/16929.py
@@ -0,0 +1,40 @@
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+
+def isin(y, x):
+    if 0<=y<N and 0<=x<M:
+        return True
+    return False
+def backtracking(visited, startingpoint, y, x, arr, alpha, count):
+
+    for i in range(4):
+        nexty, nextx = dy[i]+y, dx[i]+x
+        if(isin(nexty, nextx)):
+            if [nexty, nextx] == startingpoint and count>3:
+                return True
+            if not visited[nexty][nextx] and arr[nexty][nextx] == alpha:
+                visited[nexty][nextx] = True
+                if backtracking(visited, startingpoint, nexty, nextx, arr, alpha, count+1):
+                    return True
+                visited[nexty][nextx] = False
+    
+    return False
+
+
+def solution(arr, N, M):
+    
+    for i in range(N):
+        for j in range(M):
+            visited = [[False]*M for _ in range(N)]
+            visited[i][j] = True
+            if backtracking(visited, [i, j], i, j, arr, arr[i][j], 1):
+                return "Yes"
+
+    return "No"
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    arr = []
+    for i in range(N):
+        arr.append(list(input()))
+    
+    print(solution(arr, N, M))
